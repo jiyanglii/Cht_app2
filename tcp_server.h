f5dfe142f57379f962963f5471a3b747d809a32c
Dingran Wang <dingran.wang@motorolasolutions.com>
2018-10-07 00:32:08 -0500

    - save - server: add client list

--- tcp_server.h
+++ tcp_server.h
@@ -1,10 +1,29 @@
 
 #pragma once
 
-#define BACKLOG 5
-#define STDIN 0
-#define TRUE 1
-#define CMD_SIZE 100
+#define BACKLOG     5
+#define STDIN       0
+#define TRUE        1
+#define CMD_SIZE    100
 #define BUFFER_SIZE 256
+#define MAX_CLIENT  0xff
+
+#define LOGGED_IN       1
+#define LOGGED_OUT      0
+
+
+struct s_client{
+    int client_id;
+    char status;
+    char host_name[20];
+    uint32_t ip;
+    int port_num;
+    int msg_rev;
+    int msg_sent;
+    int fd;
+    struct sockaddr_in client_info;
+};
 
 int tcp_server(int s_PORT);
+int new_client(int fd, struct sockaddr*);
+void *get_in_addr(struct sockaddr *sa);
