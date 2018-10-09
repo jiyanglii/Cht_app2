c83225d4cefcb6eb70f934a160e9b1a14ffa0d0a
Jiyang Li <jiyang725@gmail.com>
2018-10-09 00:11:19 -0400

    move remote_server_addr.sin_family

--- tcp_client.c
+++ tcp_client.c
@@ -145,15 +145,15 @@ int connect_to_host(char *server_ip, int server_port)
     fdsocket = socket(AF_INET, SOCK_STREAM, 0);
     if(fdsocket < 0)
         perror("Failed to create socket");
-
+    remote_server_addr.sin_family = AF_INET;
     remote_server_addr.sin_port = htons(local_port);
+    remote_server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     if(bind(fdsocket, (struct sockaddr *)&remote_server_addr, (socklen_t)sizeof(struct sockaddr_in)) < 0 )
         perror("Bind failed");
 
     printf("Client: local port %08d\n", htons(local_port));
 
     bzero(&remote_server_addr, sizeof(remote_server_addr));
-    remote_server_addr.sin_family = AF_INET;
     inet_pton(AF_INET, server_ip, &remote_server_addr.sin_addr); //Convert IP addresses from human-readable to binary
     remote_server_addr.sin_port = htons(server_port);
 
