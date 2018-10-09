d2262b0961fb198c545c1c2707fe5e9cc06f1ae2
Dingran Wang <dingran.wang@motorolasolutions.com>
2018-10-08 21:58:39 -0500

    - save

--- cmdTokenizer.c
+++ cmdTokenizer.c
@@ -9,13 +9,15 @@
 #include <unistd.h>
 #include "cmdTokenizer.h"
 
-//#define DEBUG
+#define DEBUG
 
 void cmdTokenizer(char *input, struct s_cmd * parse_cmd){
 
     // parse input cmd, initialize new buffer for cmd and each arges, and pass the pointers to parse_cmd as returned parsed cmd
     char *token;
     const char delim = ' ';
+
+    parse_cmd->arg_num = 0;
 
     /* get the first token */
     token = strtok(input, &delim);
@@ -31,6 +33,9 @@ void cmdTokenizer(char *input, struct s_cmd * parse_cmd){
         token = strtok(NULL, &delim);
         parse_cmd->arg0 = token;
 
+        if(parse_cmd->arg0)
+            parse_cmd->arg_num ++;
+
 #ifdef DEBUG
         printf("Line %d: %s\n",  __LINE__, parse_cmd->arg0);
 #endif
@@ -41,9 +46,16 @@ void cmdTokenizer(char *input, struct s_cmd * parse_cmd){
         token = strtok(NULL, &delim);
         parse_cmd->arg1 = token;
 
+        if(parse_cmd->arg1)
+            parse_cmd->arg_num ++;
+
 #ifdef DEBUG
         printf("Line %d: %s\n",  __LINE__, parse_cmd->arg1);
 #endif
    }
+
+#ifdef DEBUG
+    printf("Line %d: Number of atgs in this cmd: %d\n",  __LINE__, parse_cmd->arg_num);
+#endif
 
 }
--- tcp_client.h
+++ tcp_client.h
@@ -3,12 +3,13 @@
 
 #define BACKLOG     5
 #define STDIN       0
-#define TRUE        1
+#define TRUE        true
 #define CMD_SIZE    100
 #define BUFFER_SIZE 256
 #define MSG_SIZE    256
 #define BUFFER_SIZE 256
 #define IP_SIZE     255
+#define LOGOUT      "LOGOUT"
 
 
 int tcp_client(int c_PORT);
