990c34cb692babc60c9f20ccbe4b64cb795571e9
Dingran Wang <dingran.wang@motorolasolutions.com>
2018-10-12 16:18:07 -0500

    - broadcast update

--- tcp_client.c
+++ tcp_client.c
@@ -219,7 +219,7 @@ void c_processCMD(struct s_cmd * parse_cmd, int fd){
       cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
       cse4589_print_and_log("I,%s,have read and understood the course academic integrity policy.\n",your_ubit_name);
     }
-    else if(strcmp(cmd, "LOGIN") == 0)
+    else if((strcmp(cmd, "LOGIN") == 0) && (parse_cmd->arg_num >= 2))
     {
         if(INIT_LOGIN){
             // This is ran by start up, first to establish connection to server
